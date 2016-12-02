d8c36b6a25d2611dfb732096033e1f83bd272d23
Nicolas Fleurot <nicolas.fleurot@utbm.fr>
2016-12-02 15:40:31 +0100

    Lot of fuckery

---
+++ device.mk
@@ -0,0 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS := device/utbm/green_pepe/overlay
+PRODUCT_NAME := green_pepe
+PRODUCT_DEVICE := green_pepe
+
+include $(call all-subdir makefiles)
--- green_pepe.mk
+++ green_pepe.mk
@@ -1,6 +1,11 @@
 $(call inherit-product, device/linaro/pandaboard/pandaboard.mk)
 
+DEVICE_PACKAGE_OVERLAYS := device/utbm/green_pepe/overlay
+PRODUCT_COPY_FILE := overlay/system/media/bootanimation.zip:system/media/bootanimation.zip
+
 PRODUCT_NAME:= green_pepe
 PRODUCT_DEVICE:= green_pepe
 PRODUCT_BRAND:= Android
 PRODUCT_MODEL:= Android
+
+include $(call all-subdir makefiles)
